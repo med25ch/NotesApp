49b4f8dca9e545e46e18d7db4ff9f483100b2482
med25ch <1804233@bdeb.qc.ca>
2022-11-27 17:06:28 -0500

    initial commit

---
+++ app/src/main/java/com/resse/notesapp/ListFragment.kt
@@ -0,0 +1,30 @@
+package com.resse.notesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+class ListFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener{
+            findNavController().navigate(R.id.addFragment)
+        }
+
+        return view
+    }
+
+
+}
